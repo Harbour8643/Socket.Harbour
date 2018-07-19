5843bbfc19b0b922f5f63a086d77fb14f67867fd
Harbour <864353016@qq.com>
2018-07-19 13:00:39 +0800

    完善

--- SocketLibrary/Properties/AssemblyInfo.cs
+++ SocketLibrary/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SocketLibrary")]
 [assembly: AssemblyDescription("SocketLibrary")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Harbour")]
+[assembly: AssemblyCompany("Harbour<864353016@qq.com>")]
 [assembly: AssemblyProduct("SocketLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("Harbour")]
+[assembly: AssemblyTrademark("HarbourHarbour<864353016@qq.com>")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
